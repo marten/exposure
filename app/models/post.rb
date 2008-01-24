0182faf00e6c26823d57461a888a2db7d80553ad
Marten Veldthuis <marten@veldthuis.com>
2008-01-24 16:56:47 +0100

    Some style changes in the comments page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments, :dependent => :destroy
+  has_many :comments, :dependent => :destroy, :order => "created_at ASC"
   
   validates_presence_of :title, :caption, :photo
   
