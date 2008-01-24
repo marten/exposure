744fd1b4eb42c1e095f7301085b098f2de22e60c
Marten Veldthuis <marten@veldthuis.com>
2008-01-24 17:23:31 +0100

    Don't trust user input.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def markup(s)
+    # replacing newlines to <br> ans <p> tags
+    # wrapping text into paragraph
+    s = "<p>" + s.gsub(/\n\n+/, "</p>\n\n<p>").
+                  gsub(/([^\n]\n)(?=[^\n])/, '\1<br />') + "</p>"
+  end
+  
 end
